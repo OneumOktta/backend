7d886b0c1527702889c742f5640a1625d7764232
danpilnet <dnpdnp28@gmail.com>
2024-11-28 00:32:42 +0900

    add models

--- oktta/users/models.py
+++ oktta/users/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUsers(BaseUserManager):
+
+    def create_user(self,
+                    id = models.IntegerField,
+                    email = models.EmailField(unique=True),
+                    password = models.CharField(max_length=100),
+                    role = models.CharField(max_length=250),
+                    managers = models.CharField(max_length=250),
+                    managers_accept = models.BooleanField(default=False),
+                    managers_email = models.EmailField):
+        return self.create_user(id,
+                                email,
+                                password,
+                                role,
+                                managers,
+                                managers_accept,
+                                managers_email)
