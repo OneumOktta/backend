5e8a9b91ad77123597a286e4f50543508f229aff
Andrey <v.andrei.dmitrievich@gmail.com>
2024-11-28 15:13:31 +0300

    order model

--- oktta/order/models.py
+++ oktta/order/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+
+class Order(models.Model):
+    """Модель для заявок на демонстрацию сервиса"""
+
+    STATUS_CHOICES = [
+        (True, 'Активен'),
+        (False, 'Неактивен')
+    ]
+
+    full_name = models.CharField(max_length=100, verbose_name='ФИО')
+    email = models.EmailField(db_index=True, unique=True, max_length=254, verbose_name='Почта')
+    phone = models.CharField(unique=True, max_length=20, verbose_name='Номер телефона')
+    company_name = models.CharField(max_length=255, verbose_name='Название компании')
+    site = models.URLField(max_length=255, verbose_name='Сайт')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    status = models.BooleanField(default=True, choices=STATUS_CHOICES, verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.full_name} - {self.email}({self.phone})'
