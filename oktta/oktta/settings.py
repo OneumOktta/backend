e7cadbf88773f5933f509a1b12e0ae60d344545a
danpilnet <dnpdnp28@gmail.com>
2024-12-04 00:50:41 +0900

    add user

--- oktta/oktta/settings.py
+++ oktta/oktta/settings.py
@@ -70,8 +70,6 @@ DATABASES = {
     }
 }
 
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- oktta/users/models.py
+++ oktta/users/models.py
@@ -1,4 +1,5 @@
 from enum import unique
+from tabnanny import verbose
 
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
@@ -18,42 +19,36 @@ class ManagerBase(BaseUserManager):
     def create_superuser(self, email, password=None):
 
         user = self.create_user(email, password=password)
-
-        user.is_admin = True
+        user.role = 'administrator'
         user.save(using=self._db)
         return user
 
 
 class CustomUsers(AbstractBaseUser):
-    # CHOICES_ROLE = [
-    #     (admin, 'AD')
-    # ]
 
-    email = models.EmailField(unique=True, max_length=254, verbose_name='email')
-    password = models.CharField(max_length=75, verbose_name='password')
-    role = models.CharField(max_length=100, verbose_name='role')
+    CHOICES_ROLE = [('administrator', 'Администратор'),
+                    ('business', 'Бизнес'),
+                    ('manager', 'Менеджер')]
+
+    CHOICES_ACTIVE = [(True, 'Активирован'),
+                      (False, 'Не активирован')
+                      ]
+
+    email = models.EmailField(db_index=True, unique=True, max_length=254, verbose_name='Почта')
+    password = models.CharField(max_length=75, verbose_name='Пароль')
+    role = models.CharField(db_index=True, max_length=15, choices=CHOICES_ROLE, default='business', verbose_name='Роль')
+    user_active = models.BooleanField(choices=CHOICES_ACTIVE, default=False, verbose_name='Активирован')
+    company_name = models.CharField(blank=True, max_length=100, verbose_name='Название компании')
+    name = models.CharField(blank=True, max_length=100, verbose_name='Имя')
+    phone = models.CharField(blank=True, max_length=20, verbose_name='Номер телефона')
+    telegram = models.BooleanField(db_index=True, default=False, verbose_name='Телеграм')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 
     objects = ManagerBase()
 
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['password', ]
     USERNAME_FIELD = 'email'
 
     def __str__(self):
-        return self.email
-
-    def has_perm(self, perm, obj=None):
-        return False
-
-    def has_module_perms(self, app_label):
-        return False
-
-
-class UserApiKey(models.Model):
-    STATUS = [
-        (True, 'Подтвержден'),
-        (False, 'Не подтвержден')
-    ]
-
-    is_active = models.BooleanField(default=False, choices=STATUS, verbose_name='Подтверждение почты')
-    email = models.EmailField(unique=True, verbose_name='email')
-
+        return f'{self.email}'
