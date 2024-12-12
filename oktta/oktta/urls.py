32fa909a2da5619f61d108e7ed4c7c3aa2e7044f
Andrey <v.andrei.dmitrievich@gmail.com>
2024-12-12 22:50:45 +0300

    added jwt

--- oktta/oktta/urls.py
+++ oktta/oktta/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('order.urls')),
     path('api/', include('users.urls')),
-    path('api/swagger/', TemplateView.as_view(template_name='swagger_ui.html'))
+    path('api/swagger/', TemplateView.as_view(template_name='swagger_ui.html')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
